fb24cd960bb708b76f0bbbdcd93945ec629657c3
neilmac1974 <129184164+neilmac1974@users.noreply.github.com>
2023-03-30 10:34:30 +0000

    ReturnedBooks

--- notes.sql
+++ notes.sql
@@ -1,7 +1,18 @@
-SELECT Patrons.FirstName, Patrons.Email, Books.Title, Loans.DueDate
-FROM Loans
-JOIN Books on Loans.BookID = Books.BookID
-JOIN Patrons on Loans.PatronID = Patrons.PatronID
-WHERE Loans.DueDate = '2022-07-13'
-AND Loans.ReturnedDate IS NULL
 
+UPDATE Loans
+SET ReturnedDate = '2022-07-05'
+WHERE BookID =
+(SELECT BookID FROM Books WHERE Barcode = '6435968624' AND Loans.ReturnedDate IS NULL);
+
+UPDATE Loans
+SET ReturnedDate = '2022-07-05'
+WHERE BookID =
+(SELECT BookID FROM Books WHERE Barcode = '5677520613' AND Loans.ReturnedDate IS NULL);
+
+UPDATE Loans
+SET ReturnedDate = '2022-07-05'
+WHERE BookID =
+(SELECT BookID from Books WHERE Barcode = '8730298424' AND Loans.ReturnedDate IS NULL);
+
+
+SELECT * FROM Loans WHERE ReturnedDate = '2022-07-05';
